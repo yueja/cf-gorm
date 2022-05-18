1ae48a2568600479cdd892d11e97116fb39cc489
yuejianan <10405526+yueja@user.noreply.gitee.com>
2022-05-18 17:48:42 +0800

    🎸feat: 事务支持

--- mysql/db/tx.go
+++ mysql/db/tx.go
@@ -24,3 +24,28 @@ func WithTransaction(fn func(tx *Curd) error) error {
 	}
 	return nil
 }
+
+// WithTransactionSpecify 事务支持
+func WithTransactionSpecify(client string, fn func(tx *Curd) error) error {
+	var err error
+	tx := SpecifyDbCurd(client).db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p) // re-throw panic after Rollback
+		}
+		if err != nil {
+			tx.Rollback() // err is non-nil; don't change it
+		} else {
+			tx.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+	err = fn(&Curd{db: tx})
+	if err != nil {
+		return err
+	}
+	return nil
+}
